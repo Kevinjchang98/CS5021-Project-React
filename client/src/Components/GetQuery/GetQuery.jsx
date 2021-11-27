9e9c2b57df3de6edc6159aa69e5b4d5f67af3235
kevinchang <kevinjchang98@gmail.com>
2021-11-27 00:23:54 -0800

    remove data on page change

--- client/src/Components/GetQuery/GetQuery.jsx
+++ client/src/Components/GetQuery/GetQuery.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { JsonToTable } from 'react-json-to-table';
 
 function GetQuery(props) {
 	const [ data, setData ] = useState();
+
+	useEffect(
+		() => {
+			setData();
+		},
+		[ props.title ]
+	);
 
 	const query = () => {
 		Axios.get('https://cs5021-project.herokuapp.com/' + props.url) //
@@ -14,6 +21,8 @@ function GetQuery(props) {
 	return (
 		<div className="GetQueryBody">
 			<h1>{props.title}</h1>
+
+			<p>{props.description}</p>
 
 			<button
 				onClick={() => {
