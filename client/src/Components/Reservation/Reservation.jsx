3389caf3ebfe5f664664eeffc10b7be94863c889
kevinchang <kevinjchang98@gmail.com>
2021-11-28 00:30:16 -0800

    change loading icon to TailSpin

--- client/src/Components/Reservation/Reservation.jsx
+++ client/src/Components/Reservation/Reservation.jsx
@@ -60,7 +60,7 @@ function Reservation() {
 
 			{isLoading ? (
 				<div className="LoadIcon">
-					<Loader type="ThreeDots" color="#1D1D1D" height={80} width={80} />
+					<Loader type="TailSpin" color="#1D1D1D" height={80} width={80} />
 				</div>
 			) : (
 				<button
