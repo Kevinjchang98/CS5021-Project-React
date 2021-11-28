38d1698db73b9b108b4be67afd1e9a2577cc3eb0
kevinchang <kevinjchang98@gmail.com>
2021-11-28 02:23:12 -0800

    set sidebar width to 30vw, max of 400px, wrap in SidebarWrapper class

--- client/src/Components/Sidebar/Sidebar.jsx
+++ client/src/Components/Sidebar/Sidebar.jsx
@@ -6,46 +6,48 @@ import queryData from '../GetQuery/GetQueryData';
 
 function Sidebar() {
 	return (
-		<ProSidebar width="400px">
-			<SidebarHeader>
-				<div
-					style={{
-						padding: '20px',
-						textTransform: 'uppercase',
-						fontWeight: 'bold',
-						fontSize: 14,
-						letterSpacing: '1px',
-						overflow: 'hidden',
-						textOverflow: 'ellipsis',
-						whiteSpace: 'nowrap'
-					}}
-				>
-					CS5021-1 Group 7
-					<br />
-					Flight School Management Database
-				</div>
-			</SidebarHeader>
-			<Menu>
-				<MenuItem>
-					<Link to="/">Home</Link>
-				</MenuItem>
-				<MenuItem>
-					<Link to="/reservation">Create Reservation</Link>
-				</MenuItem>
-				<SubMenu title="Sample Queries" defaultOpen="true">
+		<div className="SidebarWrapper">
+			<ProSidebar width="100%">
+				<SidebarHeader>
+					<div
+						style={{
+							padding: '20px',
+							textTransform: 'uppercase',
+							fontWeight: 'bold',
+							fontSize: 14,
+							letterSpacing: '1px',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+							whiteSpace: 'nowrap'
+						}}
+					>
+						CS5021-1 Group 7
+						<br />
+						Flight School Management Database
+					</div>
+				</SidebarHeader>
+				<Menu>
 					<MenuItem>
-						<Link to="/recommended-aircraft">Recommended Aircraft</Link>
+						<Link to="/">Home</Link>
 					</MenuItem>
-					{queryData.map((data) => {
-						return (
-							<MenuItem key={data.url}>
-								<Link to={'/' + data.url}>{data.title}</Link>
-							</MenuItem>
-						);
-					})}
-				</SubMenu>
-			</Menu>
-		</ProSidebar>
+					<MenuItem>
+						<Link to="/reservation">Create Reservation</Link>
+					</MenuItem>
+					<SubMenu title="Sample Queries" defaultOpen="true">
+						<MenuItem>
+							<Link to="/recommended-aircraft">Recommended Aircraft</Link>
+						</MenuItem>
+						{queryData.map((data) => {
+							return (
+								<MenuItem key={data.url}>
+									<Link to={'/' + data.url}>{data.title}</Link>
+								</MenuItem>
+							);
+						})}
+					</SubMenu>
+				</Menu>
+			</ProSidebar>
+		</div>
 	);
 }
 
